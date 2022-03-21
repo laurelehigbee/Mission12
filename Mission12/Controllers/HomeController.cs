e17ce5c774ad40b9cd24af886543f707f264d793
laurelehigbee <90651214+laurelehigbee@users.noreply.github.com>
2022-03-21 10:20:48 -0600

    Add project files.

---
+++ Mission12/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission12.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission12.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
