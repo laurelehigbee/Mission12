9ae45c9bb29bbc0c603736954a8b7b12f4196697
laurelehigbee <90651214+laurelehigbee@users.noreply.github.com>
2022-03-21 17:48:36 -0600

    Added Appointments page, adjusted controller, prep work for view appointments model

--- Mission12/Controllers/HomeController.cs
+++ Mission12/Controllers/HomeController.cs
@@ -11,12 +11,17 @@ namespace Mission12.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index() //what to return when Index page is requested
         {
             return View();
         }
 
-        public IActionResult SignUp()
+        public IActionResult SignUp() //what to return when SignUp page is requested
+        {
+            return View();
+        }
+
+        public IActionResult ViewAppointments() //what to return when ViewAppointments page is requested
         {
             return View();
         }
