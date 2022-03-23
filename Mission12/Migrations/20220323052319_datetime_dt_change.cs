9cdf28cde96ba23bc27d0990d09c974262028292
Shad <shadwritescode@gmail.com>
2022-03-22 23:49:56 -0600

    Updates to model datatypes. WIP navigation

---
+++ Mission12/Migrations/20220323052319_datetime_dt_change.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission12.Migrations
+{
+    public partial class datetime_dt_change : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.UpdateData(
+                table: "Responses",
+                keyColumn: "TourId",
+                keyValue: 1,
+                columns: new[] { "AptDate", "AptTime" },
+                values: new object[] { new DateTime(2022, 3, 22, 23, 23, 18, 856, DateTimeKind.Local).AddTicks(8920), 20 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+    
+
+            migrationBuilder.UpdateData(
+                table: "responses",
+                keyColumn: "TourId",
+                keyValue: 1,
+                columns: new[] { "AptDate", "AptTime" },
+                values: new object[] { "January 1 2022", "12:00 pm" });
+        }
+    }
+}
